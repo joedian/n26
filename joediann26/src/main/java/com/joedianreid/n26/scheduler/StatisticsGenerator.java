053d50fbcb8ffbe973e7ab07732bea22e2c3e63e
joedian <joedian_reid@yahoo.com>
2018-03-25 22:04:07 -0600

    updated scheduler

--- joediann26/src/main/java/com/joedianreid/n26/scheduler/StatisticsGenerator.java
+++ joediann26/src/main/java/com/joedianreid/n26/scheduler/StatisticsGenerator.java
@@ -10,7 +10,6 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.joedianreid.n26.models.Statistic;
-import com.joedianreid.n26.models.Transaction;
 import com.joedianreid.n26.services.StatisticService;
 
 @Component
@@ -24,7 +23,7 @@ public class StatisticsGenerator {
 	
 	private static final Logger log = LoggerFactory.getLogger(StatisticsGenerator.class);
 
-    @Scheduled(fixedRate = 5000)//run it every five seconds
+    @Scheduled(fixedRate = 1000)//run it every second
     public void generateCurrentStatistics() {
     	 System.out.println("Current timestamp: " +  Instant.now().atOffset(ZoneOffset.UTC ).toInstant().toEpochMilli());
         statisticService.updateStatistics();
@@ -40,13 +39,13 @@ public class StatisticsGenerator {
      * 2. add transaction
      * 3. custom intervals
      */
-    @Scheduled(fixedRate = 1000)//run it every second
+   /* @Scheduled(fixedRate = 1000)//run it every second
     public void generateTransactions() {
     	Transaction transaction = new Transaction();
     	Double rand = Math.random();
     	transaction.setAmount(Math.floor(5D * rand * 10)); //random number generation
     	transaction.setTimestamp( Instant.now().atOffset(ZoneOffset.UTC ).toInstant().toEpochMilli());
         statisticService.addToQue(transaction);
-    }
+    }*/
 
 }
